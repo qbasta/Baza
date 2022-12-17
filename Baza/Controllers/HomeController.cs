6f90af6336043b5fee9ced190c7684dbcc5e03f8
Radek2894 <85341631+Radek2894@users.noreply.github.com>
2022-12-17 22:00:41 +0100

    Dodanie opcji ogłoszenia, frontendowe rzeczy

--- Baza/Controllers/HomeController.cs
+++ Baza/Controllers/HomeController.cs
@@ -49,6 +49,11 @@ namespace Baza.Controllers
             return View();
         }
 
+        public IActionResult Announcements()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
